dabcb6e773d36e4339e3b6f1675fc53ddcfe2414
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-15 16:34:33 -0800

    added Coin prices on charts page

--- src/index.js
+++ src/index.js
@@ -6,20 +6,36 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 import Form from './components/Form';
+import Info from './components/Info';
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [allExchanges, setAllExchanges] = useState([]);
 
-
+  useEffect(() => {
+    axios
+    .get('https://api.coingecko.com/api/v3/exchanges/list')
+    .then(res => {
+      // console.log(res.data)
+      setAllExchanges(res.data);
+    })
+    .catch(err => {
+      alert('error loading exchanges list', err)
+      console.log('error loading exchanges list', err)
+    })
+  })
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        setCoinData(res.data)
+        // console.log(res.data)
+      })
       .catch(err => console.log(err));
   }, []);
   return (
@@ -27,6 +43,7 @@ const App = () => {
       <Route path='/' component={Navbar} />
       <Route exact path='/' component={Form} />
       <Route path='/charts' render={() => <Charts coinData={coinData} />} />
+      <Route path='/info' render={() => <Info allExchanges={allExchanges} />} />
       
     </div>
   );
